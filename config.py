f08410302ed78845f930b50f1fc4648c014d52d2
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-09-08 15:08:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #update for req channel
-SESSION_STRING = os.environ.get("SESSION_STRING", "AQCIqBkApkfgwUtmbnkIA9HejcdCS63-txknP6QBj4xpWiSJr77FhHBPgtn1UGylLjyq0gnMlsYzYRdbeJwcY1W2wBa3IA_CLSEty6CLvPfbVjXpd_PeRHXHb98CkenYohyMDhAbLv4JTf7GoWFKzF1E6TcyJJaV-YCNwgw-S2MNY48V7-1TWI37Wnb9CddGbnAVSXzYPRDverV4Zw8ytQ5Xgc-l72cdKnH6B1zwJRhjAwdS-US2ezbHUGLODC8ARzQrwtMAA2LcnEShYv-ugtaTN2cA_30jjeqaBHVjuT9ZFlkYfX7XX2o5C6s5iGTWBCTMk2E42AEv1ueRT6X9CpcJHyftYQAAAAB4DBqcAA")
-REQUEST_CHANNEL_ID = int(os.environ.get("REQUEST_CHANNEL_ID", "-1001818783065"))
-REQUEST_CHANNEL_LINK = os.environ.get("REQUEST_CHANNEL_LINK", "https://t.me/+CpOSmVrewr5mMDRl")
+SESSION_STRING = os.environ.get("SESSION_STRING", "BQA1ENIAMcfNEIS2NxnMj1ZQOrg6-tkCZmz2hrwmyyLS9kwNMv5op504B3ok1mEUqGTAr-Xdxd5pyr5o0onmKZWQ8oRS5ZN_kOpYxJzNWAivWsQrUvnbZj12SP9wdLPzwkDoEhW2mXVGoRR8T8unJ6CWV_II7hj_LqEFeKZen_XlAX6Qv_uCeAF28NdFTLSh7zxlz2xTm5KxIa0InVc-r34lyp3IMjVs7uS-7FdU1GInjgKWsBQ2kwlvUc3vJmaU-rcOZtXTXemm2vfea8og3ej7m_K-4UNmNaBniV6Yi5DaNMcnxM2iW5oJBdsT7wfZU_2TlK5-VwPTnDKhO__1Z8Sblhf7vQAAAABpspdLAA")
+REQUEST_CHANNEL_ID = int(os.environ.get("REQUEST_CHANNEL_ID", "-1001978977216"))
+REQUEST_CHANNEL_LINK = os.environ.get("REQUEST_CHANNEL_LINK", "https://t.me/+E_9j3fM54MZlZGQ1")
 
 
 #Bot token @Botfather
@@ -23,20 +23,20 @@ APP_ID = int(os.environ.get("APP_ID", "3477714"))
 API_HASH = os.environ.get("API_HASH", "1264d2d7d397c4635147ee25ab5808d1")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001971965786"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001593914815"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5672110846"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1773311819"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://colab:colab@colab.okoqygn.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "shikarikabot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001818783065"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001978977216"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -46,7 +46,7 @@ REQUEST_JOIN_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1773311819").split()):
+    for x in (os.environ.get("ADMINS", "1979647570").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
